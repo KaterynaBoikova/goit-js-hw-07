f13222f2521169fc0d92092ca5aac3edd4405131
Kateryna Boikova <kateboikova@gmail.com>
2020-12-19 22:57:44 +0200

    fixed task5

--- js/task-5.js
+++ js/task-5.js
@@ -1,6 +1,11 @@
 const nameInputRef = document.querySelector('#name-input');
 const nameOutputRef = document.querySelector('#name-output');
 
+
 nameInputRef.addEventListener('input', (event) => {
     nameOutputRef.textContent = event.target.value;
+    if(nameOutputRef.textContent === ''){
+        nameOutputRef.textContent = 'незнакомец';
+    }
 });
+
